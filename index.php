0f3a0791bf8c46d427f0cccd27cff86825bf2274
Wasim Shinkada <[w.shinkada@asysgmbh.de]>
2018-01-19 14:19:35 +0100

    u

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ and open the template in the editor.
 	    return $myString;
 	}
 	
-	echo myFunction('this is test function to test the Git');
+	echo myFunction('<a class="mylink" href="#">this is test function to test the Git</a>');
 	?>
     </body>
 </html>
